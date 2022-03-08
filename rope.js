546ac092c4209e13524b2523d01643865f50af9a
leonardoaranhabyjus <leonardo.aranha@byjusfutureschool.com>
2022-03-08 16:16:21 -0300

    init

---
+++ rope.js
@@ -0,0 +1,106 @@
+class Rope
+  {
+    //o construtor recebe quantos seguimentos a corda tem pelo "nlink" e a posição X/Y pelo "pointA"
+    constructor(nlink, pointA)
+    {
+      this.nlink = nlink
+
+  //cria um grupo de "corpos", para depois adicionar colisão em cada um
+  const group = Body.nextGroup(true);
+
+  //cria cada vertice da corda separadamente usando retangulos
+  const rects = Composites.stack(100, 100, this.nlink, 1, 5, 5, function(x, y) {
+      return Bodies.rectangle(x, y, 30, 5, { collisionFilter: { group: group } });
+  });
+      
+  this.pointA = pointA;
+
+  //junta cada vertice da corda
+  this.body = Composites.chain(rects, 0.1, 0, -0.6, 0, {stiffness: 0.1, length: 0.1, render: {type: 'line'}});
+  
+  //cria o "mundo" (fisica, etc) 
+  World.add(engine.world, this.body);
+
+  //coloca o grupo de vertices no "mundo"
+    Composite.add(rects, Constraint.create({
+    pointA: this.pointA,
+    bodyB: rects.bodies[0],
+    pointB: {x: -25, y: 0},
+    length:10,
+    stiffness: 0.1
+  }));
+      
+    }
+    
+    break()
+    { //Matter.Composite.clear(this.rope,true);
+      this.body = null;
+    }
+    
+    show()
+    {
+      if(this.body!=null)
+        {
+          //manda cada vertice para a função drawVertices
+          for (let i = 0; i < this.body.bodies.length-1; i++)
+          {
+              this.drawVertices(this.body.bodies[i].vertices);
+             }
+        }
+    }
+    //desenha todos os vertices
+    drawVertices(vertices) 
+    {
+      beginShape();
+      //cor de cada vertice
+      fill('#FFF717')
+      //sem borda
+      noStroke();
+      
+      //desenha todos vertices
+      for (let i = 0; i < vertices.length; i++) 
+      {
+       vertex(vertices[i].x, vertices[i].y);
+       }
+      endShape(CLOSE);
+   }
+
+   //essa função não é utilizada nesse projeto
+   showConstraints(constraints) 
+   {
+     if(constraints!=null)
+     {
+    for (let i = 0; i < constraints.length; i++) {
+      this.drawConstraint(constraints[i]);
+    }
+  }
+  }
+
+  //essa função não é utilizada nesse projeto
+  drawConstraint(constraint) {
+    if(constraint!=null)
+      {
+    const offsetA = constraint.pointA;
+    let posA = {x:0, y:0};
+    if (constraint.bodyA) {
+      posA = constraint.bodyA.position;
+    }
+    const offsetB = constraint.pointB;
+    let posB = {x:0, y:0};
+    if (constraint.bodyB) {
+      posB = constraint.bodyB.position;
+    }
+    push()
+    strokeWeight(4);
+    stroke(255);
+    line(
+      posA.x + offsetA.x,
+      posA.y + offsetA.y,
+      posB.x + offsetB.x,
+      posB.y + offsetB.y
+    );
+    pop();
+      }
+  }  
+    
+  }
---
+++ sketch.js
@@ -0,0 +1,63 @@
+const Engine = Matter.Engine;
+const Render = Matter.Render;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Constraint = Matter.Constraint;
+const Body = Matter.Body;
+const Composites = Matter.Composites;
+const Composite = Matter.Composite;
+
+let engine;
+let world;
+var ground;
+var fruit,rope;
+var fruit_con;
+
+function setup() 
+{
+  //cria o "fundo"
+  createCanvas(500,700);
+  frameRate(80);
+  engine = Engine.create();
+  world = engine.world;
+  //cria o chão
+  ground = new Ground(200,680,600,20);
+
+  //cria uma nova corda com 7 segmentos e na posição X/Y
+  rope = new Rope(10,{x:245,y:30});
+
+  //cria a "bola"
+  fruit = Bodies.circle(300,300,20);
+  
+  //adiciona fisica usando o matter.js
+  Matter.Composite.add(rope.body,fruit);
+
+  //cria um link entre a corda e a bola
+  fruit_con = new Link(rope,fruit);
+
+  rectMode(CENTER);
+  ellipseMode(RADIUS);
+  textSize(50)
+  
+}
+
+function draw() 
+{
+  //cor do "fundo"
+  background(51);
+
+  //desenha a corda
+  rope.show();
+
+  //desenha a "bola" com o tamanho X=30 e Y=30
+  ellipse(fruit.position.x,fruit.position.y,30,30);
+
+  //atualiza em tempo real a fisica
+  Engine.update(engine);
+
+  //desenha o chão
+  ground.show();
+
+ 
+   
+}
