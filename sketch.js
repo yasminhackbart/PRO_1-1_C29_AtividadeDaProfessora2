f586109bf0792033b6c86cd92c67fc324d58b701
Yasmin Hackbart <90115526+yasminhackbart@users.noreply.github.com>
2022-12-13 17:39:51 -0300

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -24,16 +24,16 @@ function setup()
   ground = new Ground(200,680,600,20);
 
   //cria uma nova corda com 7 segmentos e na posição X/Y
-  rope = new Rope(10,{x:245,y:30});
+
 
   //cria a "bola"
-  fruit = Bodies.circle(300,300,20);
+
   
   //adiciona fisica usando o matter.js
-  Matter.Composite.add(rope.body,fruit);
+
 
   //cria um link entre a corda e a bola
-  fruit_con = new Link(rope,fruit);
+
 
   rectMode(CENTER);
   ellipseMode(RADIUS);
@@ -47,10 +47,10 @@ function draw()
   background(51);
 
   //desenha a corda
-  rope.show();
+
 
   //desenha a "bola" com o tamanho X=30 e Y=30
-  ellipse(fruit.position.x,fruit.position.y,30,30);
+
 
   //atualiza em tempo real a fisica
   Engine.update(engine);
